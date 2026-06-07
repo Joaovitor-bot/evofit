d76d0f33ca8734a9d27292512ce7c5e21e59292d
João Vitor <joaojotaro6@gmail.com>
2026-06-07 20:45:19 -0300

    Corrige versões do app Ionic

--- frontend-angular/src/app/pages/acompanhamento/acompanhamento.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-acompanhamento',
-  imports: [],
-  templateUrl: './acompanhamento.component.html',
-  styleUrl: './acompanhamento.component.css',
-})
-export class AcompanhamentoComponent {}
--- frontend-angular/src/app/pages/painel-aluno/painel-aluno.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-painel-aluno',
-  imports: [],
-  templateUrl: './painel-aluno.component.html',
-  styleUrl: './painel-aluno.component.css',
-})
-export class PainelAlunoComponent {}
