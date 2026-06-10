a281f87045fd85d964f7e851c0723b36a9162790
João Vitor <joaojotaro6@gmail.com>
2026-06-09 23:48:58 -0300

    Atualizando botão de editar informação do aluno

--- app-evofit/src/app/pages/alunos/alunos.page.ts
+++ app-evofit/src/app/pages/alunos/alunos.page.ts
@@ -9,10 +9,11 @@ import { AlunosService, Aluno } from '../../services/alunos.service';
 })
 export class AlunosPage implements OnInit {
   alunos: Aluno[] = [];
-  novoAluno: Aluno = { nome: '', whatsapp: '', email: '', genero: 'Masculino' };
   mostrarForm = false;
-
   generos = ['Masculino', 'Feminino', 'Outro'];
+
+  // aluno atual para cadastro/edição
+  atualAluno: Aluno = { nome: '', whatsapp: '', email: '', genero: 'Masculino' };
 
   constructor(private alunosService: AlunosService) {}
 
@@ -22,32 +23,56 @@ export class AlunosPage implements OnInit {
 
   carregarAlunos() {
     this.alunosService.listar().subscribe({
-      next: (res) => this.alunos = res,
+      next: (res) => {
+        console.log('Alunos recebidos:', res);
+        this.alunos = res;
+      },
       error: (err) => console.error('Erro ao listar alunos', err)
     });
   }
 
-  abrirForm() {
+  abrirForm(aluno?: Aluno) {
     this.mostrarForm = true;
-    this.novoAluno = { nome: '', whatsapp: '', email: '', genero: 'Masculino' };
+    if (aluno) {
+      // Edição: preenche o formulário com os dados do aluno
+      this.atualAluno = { ...aluno };
+    } else {
+      // Novo aluno
+      this.atualAluno = { nome: '', whatsapp: '', email: '', genero: 'Masculino' };
+    }
   }
 
-  cadastrarAluno() {
-    this.alunosService.cadastrar(this.novoAluno).subscribe({
-      next: () => {
-        this.carregarAlunos();
-        this.mostrarForm = false;
-      },
-      error: (err) => console.error('Erro ao cadastrar aluno', err)
-    });
+  salvarAluno() {
+    if (!this.atualAluno.id) {
+      // Cadastro
+      this.alunosService.cadastrar(this.atualAluno).subscribe({
+        next: () => {
+          this.carregarAlunos();
+          this.mostrarForm = false;
+        },
+        error: (err) => console.error('Erro ao cadastrar aluno', err)
+      });
+    } else {
+      // Edição
+      this.alunosService.editar(this.atualAluno).subscribe({
+        next: () => {
+          this.carregarAlunos();
+          this.mostrarForm = false;
+        },
+        error: (err) => console.error('Erro ao editar aluno', err)
+      });
+    }
   }
 
   excluirAluno(id?: number) {
     if (!id) return;
-
     this.alunosService.excluir(id).subscribe({
       next: () => this.carregarAlunos(),
       error: (err) => console.error('Erro ao excluir aluno', err)
     });
+  }
+
+  cancelarForm() {
+    this.mostrarForm = false;
   }
 }
