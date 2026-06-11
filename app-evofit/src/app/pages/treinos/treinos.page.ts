461bef961399162745432ecd2979fd0099a0faa5
João Vitor <joaojotaro6@gmail.com>
2026-06-11 20:20:07 -0300

    Atualizando a interface do Treino

--- app-evofit/src/app/pages/treinos/treinos.page.ts
+++ app-evofit/src/app/pages/treinos/treinos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TreinosService, Treino } from '../../services/treinos.service';
 
 @Component({
   selector: 'app-treinos',
@@ -7,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class TreinosPage implements OnInit {
+  treinos: Treino[] = [];
+  mostrarForm = false;
+  atualTreino: Treino = { aluno_id: 1, titulo: '', objetivo: '', exercicios: '', observacoes: '' };
 
-  constructor() { }
+  constructor(private treinosService: TreinosService) {}
 
   ngOnInit() {
+    this.carregarTreinos();
   }
 
+  carregarTreinos() {
+    this.treinosService.listar(this.atualTreino.aluno_id).subscribe({
+      next: (res) => this.treinos = res,
+      error: (err) => console.error('Erro ao listar treinos', err)
+    });
+  }
+
+  abrirForm(treino?: Treino) {
+    this.mostrarForm = true;
+    if (treino) this.atualTreino = { ...treino }; // edição
+    else this.atualTreino = { aluno_id: 1, titulo: '', objetivo: '', exercicios: '', observacoes: '' };
+  }
+
+  salvarTreino() {
+    if (!this.atualTreino.id) {
+      this.treinosService.cadastrar(this.atualTreino).subscribe({
+        next: () => { this.carregarTreinos(); this.mostrarForm = false; },
+        error: (err) => console.error('Erro ao cadastrar treino', err)
+      });
+    } else {
+      this.treinosService.editar(this.atualTreino).subscribe({
+        next: () => { this.carregarTreinos(); this.mostrarForm = false; },
+        error: (err) => console.error('Erro ao editar treino', err)
+      });
+    }
+  }
+
+  excluirTreino(id?: number) {
+    if (!id) return;
+    this.treinosService.excluir(id).subscribe({
+      next: () => this.carregarTreinos(),
+      error: (err) => console.error('Erro ao excluir treino', err)
+    });
+  }
+
+  cancelarForm() {
+    this.mostrarForm = false;
+  }
 }
