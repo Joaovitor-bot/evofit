d6923a99514e529067e7f3e0339bb3ec315a643e
João Vitor <joaojotaro6@gmail.com>
2026-06-17 20:21:10 -0300

    Implementa criação de acesso do aluno

--- app-evofit/src/app/pages/acesso-aluno/acesso-aluno-routing.module.ts
+++ app-evofit/src/app/pages/acesso-aluno/acesso-aluno-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { AcessoAlunoPage } from './acesso-aluno.page';
 
 const routes: Routes = [
--- app-evofit/src/app/pages/acesso-aluno/acesso-aluno.page.ts
+++ app-evofit/src/app/pages/acesso-aluno/acesso-aluno.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+import {
+  Aluno,
+  AlunosService
+} from '../../services/alunos.service';
 
 @Component({
   selector: 'app-acesso-aluno',
@@ -6,11 +12,125 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acesso-aluno.page.scss'],
   standalone: false
 })
-export class AcessoAlunoPage implements OnInit {
+export class AcessoAlunoPage {
+  alunos: Aluno[] = [];
 
-  constructor() { }
+  alunoId: number | null = null;
+  cpf = '';
+  email = '';
+  senha = '';
+  confirmarSenha = '';
 
-  ngOnInit() {
+  carregandoAlunos = false;
+  salvando = false;
+
+  erro = '';
+  sucesso = '';
+
+  constructor(
+    private alunosService: AlunosService
+  ) {}
+
+  ionViewWillEnter(): void {
+    this.carregarAlunos();
   }
 
+  carregarAlunos(): void {
+    this.carregandoAlunos = true;
+    this.erro = '';
+
+    this.alunosService.listar().subscribe({
+      next: (alunos) => {
+        this.alunos = alunos;
+        this.carregandoAlunos = false;
+      },
+
+      error: (err: HttpErrorResponse) => {
+        console.error(
+          'Erro ao carregar alunos:',
+          err
+        );
+
+        this.erro =
+          'Não foi possível carregar os alunos.';
+
+        this.carregandoAlunos = false;
+      }
+    });
+  }
+
+  criarAcesso(): void {
+    this.erro = '';
+    this.sucesso = '';
+
+    const cpfNumeros = this.cpf.replace(/\D/g, '');
+
+    if (!this.alunoId) {
+      this.erro = 'Selecione um aluno.';
+      return;
+    }
+
+    if (cpfNumeros.length !== 11) {
+      this.erro =
+        'Informe um CPF com 11 números.';
+      return;
+    }
+
+    if (!this.email.trim()) {
+      this.erro = 'Informe o e-mail do aluno.';
+      return;
+    }
+
+    if (this.senha.length < 6) {
+      this.erro =
+        'A senha deve possuir pelo menos 6 caracteres.';
+      return;
+    }
+
+    if (this.senha !== this.confirmarSenha) {
+      this.erro = 'As senhas não conferem.';
+      return;
+    }
+
+    this.salvando = true;
+
+    this.alunosService
+      .criarAcesso(
+        this.alunoId,
+        cpfNumeros,
+        this.email.trim(),
+        this.senha
+      )
+      .subscribe({
+        next: () => {
+          this.salvando = false;
+
+          this.sucesso =
+            'Acesso criado com sucesso! O aluno já pode entrar no aplicativo.';
+
+          this.limparFormulario();
+        },
+
+        error: (err: HttpErrorResponse) => {
+          console.error(
+            'Erro ao criar acesso:',
+            err
+          );
+
+          this.erro =
+            (err.error as any)?.erro ||
+            'Não foi possível criar o acesso.';
+
+          this.salvando = false;
+        }
+      });
+  }
+
+  limparFormulario(): void {
+    this.alunoId = null;
+    this.cpf = '';
+    this.email = '';
+    this.senha = '';
+    this.confirmarSenha = '';
+  }
 }
